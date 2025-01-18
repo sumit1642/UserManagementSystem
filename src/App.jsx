a73040a7b87ae17741768c730f49479bd95215ed
sumit1642 <sumeetyadav1642@gmail.com>
2025-01-18 17:04:39 +0530

    Remove SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,12 @@ import { UpdateForm } from "./components/UpdateForm";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 function App() {
 	return (
 		<div className="relative h-screen w-screen bg-slate-950">
 			<div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div>
          <div className="relative z-10">
             <Analytics />
-            <SpeedInsights />
 				<Toaster
 					position="top-center"
 					reverseOrder={false}
